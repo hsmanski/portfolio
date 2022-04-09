f6e5d1c659ceb9f23282188799f3bb5bbe74fa0f
hsmanski <hsmanski@gmail.com>
2022-04-08 22:11:42 -0700

    Show more btn functionality added

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
 const navList = document.querySelector(".nav-list");
 
+const moreBtn = document.querySelector(".more-btn");
+const btnTextToggle = document.querySelector(".more-btn span");
+const sweetTreats = document.getElementById("sweet-treats");
+const sunnyside = document.getElementById("sunnyside");
+const easyBanking = document.getElementById("easy-banking");
+
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
 
 navList.addEventListener("click", () => {
   navbar.classList.toggle("change");
+});
+
+moreBtn.addEventListener("click", () => {
+  sweetTreats.classList.toggle("hide");
+  sunnyside.classList.toggle("hide");
+  easyBanking.classList.toggle("hide");
+
+  if (sweetTreats.classList.contains("hide")) {
+    btnTextToggle.innerHTML = "More";
+  } else {
+    btnTextToggle.innerHTML = "Less";
+  }
 });
